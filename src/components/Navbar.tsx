a1be6fb59e4a7e3bc2b50625f3f1926ba8578619
Gaeuly <7dsgaeul@gmail.com>
2025-09-27 04:37:49 +0700

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,8 @@ const Navbar = () => {
   };
 
   return (
-    <header className="sticky top-0 z-50 py-4">
+    // PERUBAHAN: Mengganti 'sticky' menjadi 'absolute' dan 'w-full'
+    <header className="absolute top-0 left-0 right-0 z-50 py-4 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* === Logo === */}
